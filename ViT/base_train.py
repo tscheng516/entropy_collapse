8651d90c36355a53a2e0c3dce06fbd3f7f38140b
Tin Sum Cheng <98585214+tscheng516@users.noreply.github.com>
2026-04-14 11:17:48 +0200

    hessian frequency

--- ViT/base_train.py
+++ ViT/base_train.py
@@ -135,6 +135,7 @@ parser.add_argument("--optim", type=str)
 parser.add_argument("--lr", type=float)
 parser.add_argument("--max_it", type=int)
 parser.add_argument("--num_workers", type=int)
+parser.add_argument("--hessian_freq", type=int)
 parser.add_argument("--wandb", type=str)
 parser.add_argument(
     "--temp_shift",
@@ -164,6 +165,7 @@ _maybe_set("optimizer", known_args.optim)
 _maybe_set("learning_rate", known_args.lr)
 _maybe_set("max_iters", known_args.max_it)
 _maybe_set("num_workers", known_args.num_workers)
+_maybe_set("hessian_freq", known_args.hessian_freq)
 if known_args.wandb is not None:
     sval = str(known_args.wandb).lower()
     _maybe_set("wandb_log", sval in ("1", "true", "yes", "y"))
